362e8f112dede331fcb968ac843dff1ac61596fc
Joe Stafford <joestafford1986@gmail.com>
2018-04-18 12:04:33 +0100

    added spec file outlines

---
+++ specs/customer_spec.rb
@@ -0,0 +1,8 @@
+require("minitest/autorun")
+require("minitest/rg")
+require_relative("../pub.rb")
+require_relative("../customer.rb")
+
+class CustomerTest < MiniTest::Test
+
+end
---
+++ specs/drink_spec.rb
@@ -0,0 +1,7 @@
+require("minitest/autorun")
+require("minitest/rg")
+require_relative("../drink.rb")
+
+class DrinkTest < MiniTest::Test
+
+end
